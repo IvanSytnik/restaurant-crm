ec402c336b690057b79869d78c7d3bde81288abe
Ivan Sytnik <ivan.sytnik02@gmail.com>
2026-06-02 14:05:00 +0200

    Fix Adapter import

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import type { NextAuthOptions } from 'next-auth'
+import type { Adapter } from 'next-auth/adapters'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import bcrypt from 'bcryptjs'
